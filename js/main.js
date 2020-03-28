4dcd279086ae8877032478e22f5ca169f7ba0fac
EC2 Default User <ec2-user@ip-172-31-42-76.ec2.internal>
2020-03-28 04:27:35 +0000

    edit hover-animation sns-icons, works-masks and underline

--- js/main.js
+++ js/main.js
@@ -5,4 +5,22 @@ $('.question-icon').on('click', function(){
 
 $('.alert').on('click', function(){
   alert('準備中です。');
+});
+
+$('.works__block__image').on({
+  
+  'mouseenter' : function(){
+    console.log('mouseover');
+    $(this).siblings().children().first().addClass('underline');
+  },
+  
+  'mouseleave': function(){
+    console.log('mouseleave');
+    $(this).siblings().children().first().removeClass('underline');
+  }
+
+});
+
+$('.iconbox li').on('mouseenter', function(){
+  $(this).css({transform: 'rotate(360deg)'}, 2000);
 })

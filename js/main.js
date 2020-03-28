a502f2aaa5a60846f0eac8f69a733307fb466608
EC2 Default User <ec2-user@ip-172-31-42-76.ec2.internal>
2020-03-28 04:31:17 +0000

    modify previous commit

--- js/main.js
+++ js/main.js
@@ -10,17 +10,11 @@ $('.alert').on('click', function(){
 $('.works__block__image').on({
   
   'mouseenter' : function(){
-    console.log('mouseover');
     $(this).siblings().children().first().addClass('underline');
   },
   
   'mouseleave': function(){
-    console.log('mouseleave');
     $(this).siblings().children().first().removeClass('underline');
   }
 
 });
-
-$('.iconbox li').on('mouseenter', function(){
-  $(this).css({transform: 'rotate(360deg)'}, 2000);
-})

68bf350ddf718f0d83567089a2b190d75afe5db4
EC2 Default User <ec2-user@ip-172-31-42-76.ec2.internal>
2020-04-02 11:10:48 +0000

    edit works-section link

--- js/main.js
+++ js/main.js
@@ -1,24 +1,44 @@
 /* global $ */
+
+//skill-section ?mark-clicked
 $('.question-icon').on('click', function(){
   $('.question-box').slideToggle();
 });
 
+
+//準備中アラート
 $('.alert').on('click', function(){
   alert('準備中です。');
 });
 
+
+//works-section hover時にunderline
 $('.works__block__image').on({
-  
   'mouseenter' : function(){
-    $(this).siblings().children().first().addClass('underline');
+    $(this).siblings().children('.works__block__textbox__upper').addClass('underline');
   },
   
   'mouseleave': function(){
-    $(this).siblings().children().first().removeClass('underline');
+    $(this).siblings().children('.works__block__textbox__upper').removeClass('underline');
   }
 
 });
 
+$('.works__block__textbox__upper').on({
+  'mouseenter' : function(){
+    $(this).addClass('underline');
+  },
+  
+  'mouseleave': function(){
+    $(this).removeClass('underline');
+  }
+
+});
+
+
+
+
+//sp-view skill fadein
 $(window).scroll(function (){
   $('.fadein').each(function(){
       var elemPos = $(this).offset().top,
